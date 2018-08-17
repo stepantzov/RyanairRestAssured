aa406bdb126a9812641e841e2a6504b5ddf7ee21
stepantzov <stepantzov@gmail.com>
2018-08-17 12:31:24 +0200

    Commit 06: -added property reader utility;

--- src/main/java/auto/ryanair/utils/TimeStamp.java
+++ src/main/java/auto/ryanair/utils/TimeStamp.java
@@ -2,9 +2,16 @@ package auto.ryanair.utils;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class TimeStamp {
     public static String getTimeStampFormatted() {
-        return new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime());
+        Date currentDate = Calendar.getInstance().getTime();
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.setTime(currentDate);
+        calendar.add(Calendar.DATE, 3);
+
+        return new SimpleDateFormat("yyyy-MM-dd").format(calendar.getTime());
     }
 }
